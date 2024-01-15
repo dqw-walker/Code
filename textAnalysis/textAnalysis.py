d2a5f30162bcf7ce8b51fbe036452ca6d2f9c8c1
dengqiwen <dqw179644351@gmail.com>
2024-01-15 23:06:32 +0800

    把wordKey.txt里面的关键词导入到jieba中，然后对年报txt里的txt文件进行文本分析，得到wordKey.txt里面的关键词出现在年报txt中出现的频率

---
+++ textAnalysis/textAnalysis.py
@@ -0,0 +1,42 @@
+import jieba
+import os
+# 读取关键词
+with open('wordKey.txt', 'r', encoding='utf-8') as f:
+    keywords = f.read().splitlines()
+    # 将关键词添加到jieba的词库中
+for keyword in keywords:
+    jieba.add_word(keyword)
+    # 创建一个字典来存储关键词的频率
+    keyword_frequency = {keyword: 0 for keyword in keywords}
+
+    # 遍历annualReport目录下的所有txt文件
+for filename in os.listdir('年报txt'):
+    if filename.endswith('.txt'):
+        # 读取文件
+        with open(os.path.join('年报txt', filename), 'r', encoding='utf-8') as f:
+            text = f.read()
+        # 对文本进行分词
+        words = jieba.cut(text)
+
+        # 遍历分词结果，如果词在关键词列表中，就在字典中增加这个词的计数
+        for word in words:
+            if word in keyword_frequency:
+                keyword_frequency[word] += 1
+
+# 打印出关键词及其在文本中出现的频率
+for keyword, frequency in keyword_frequency.items():
+    print(f'{keyword}: {frequency}')
+
+    #得到以下结果
+    # 社会责任: 47
+    # 可持续发展: 20
+    # 环境友好: 0
+    # 经济利益: 235
+    # 长效发展: 0
+    # 企业治理: 0
+    # 利益共享: 1
+    # 动态管理: 0
+    # 商业模式: 78
+    # 混合组织: 0
+
+
